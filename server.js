15f8ee17cc574e878c9a8afc5a8d8c231614649e
Ashwini <076bct012.ashwini@pcampus.edu.np>
2023-09-22 11:02:01 +0545

    initial project setup

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+const pug = require("pug");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.set("view engine", "pug");
+app.use(express.static("assets"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "hello" });
+});
+
+app.listen(PORT);
+console.log("Listening on port: " + PORT);
